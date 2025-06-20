08d6c735c875c34c09bf37d4fd5bacc4db5f3c2c
Chee-En <127080897+CCH147@users.noreply.github.com>
2025-06-20 16:46:38 +0800

    added block_design (whole project)
    added*
    vivado project
    sdk C program (read BRAM data)

---
+++ Block_design/final_project/project_1.sdk/bram/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../bram_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../bram_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../bram_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../bram_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../bram_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../bram_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../bram_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../bram_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
---
+++ Block_design/final_project/project_1.sdk/bram_read/Debug/src/helloworld.d
@@ -0,0 +1,64 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xuartlite.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xuartlite_l.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../bram_read_bsp/ps7_cortexa9_0/include/xuartps_hw.h
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xuartlite.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xuartlite_l.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../bram_read_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
